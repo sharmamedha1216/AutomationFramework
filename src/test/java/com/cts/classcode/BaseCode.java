c30bac87c5118c1c90a8dd027884de22436d8cdc
Medha <sharmamedha1316@gmail.com>
2017-01-13 22:28:47 +0530

    update the files 13012017

--- src/test/java/com/cts/classcode/BaseCode.java
+++ src/test/java/com/cts/classcode/BaseCode.java
@@ -1,16 +1,23 @@
 package com.cts.classcode;
 
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BaseCode {
 	
-	WebDriver driver;
+	public WebDriver driver;
+	public WebDriverWait wdw;
 	private void launchBrowser(String browserName) {
 		if(browserName.equalsIgnoreCase("Firefox")){
-			System.setProperty("webdriver.gecko.driver", "D:\\Software\\geckodriver-v0.10.0-win64\\geckodriver.exe"); 
+			System.setProperty("webdriver.gecko.driver", "D:\\SeleniumClasses\\BrowserDrivers\\geckodriver-v0.10.0-win64\\geckodriver.exe"); 
 			driver = new FirefoxDriver();
 			
 		}
@@ -27,12 +34,16 @@ public class BaseCode {
 			System.out.println("Incorrect Browser Name");
 		}
 	}
+	Select sel;
 	
 	public void launchApplication(String url, String browserName) {
 		this.launchBrowser(browserName);
+		//implicit wait
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 		driver.get(url);
 		System.out.println("Launching Applicaiton");
 		driver.manage().window().maximize();
+		wdw= new WebDriverWait(driver,20);
 	}
 	
 	public void closeApplication(String flag) {
@@ -45,4 +56,26 @@ public class BaseCode {
 			driver.quit();
 		}
 	}
+	
+	public void selectValuesfromDropDown (WebElement element, String Text, int flag  ) {
+		sel = new Select(element);
+			if(flag==1){
+			sel.selectByVisibleText(Text);
+			}
+			else {
+				sel.selectByValue(Text);
+			}
+	}
+	
+	public void selectValuesfromDropDown (WebElement element, int ind  ) {
+		sel = new Select(element);
+		sel.selectByIndex(ind);
+	}
+	
+	public void waitForLoading(){
+		WebElement loader =driver.findElement(By.xpath(".//*[@id='loading']/img"));
+		
+		wdw.until(ExpectedConditions.visibilityOf(loader));
+	}
+	
 }
